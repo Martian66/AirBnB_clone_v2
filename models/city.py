3101077565489a4fe6723ffebbc964d6767f7f92
Martian66 <kimmartin049@gmail.com>
2023-03-21 06:49:01 -0700

    city update

--- models/city.py
+++ models/city.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
-import os
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
-    __tablename__ = 'cities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.is_type == 'db':
+        __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", passive_deletes=True, backref="cities")
+        places = relationship('Place', cascade='delete', backref='cities')
     else:
         state_id = ""
         name = ""
