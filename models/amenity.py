c8a29999b389c42efd3a2eae50e0dd25515f6aba
Martian66 <kimmartin049@gmail.com>
2023-03-21 06:38:20 -0700

    amenity_fixed

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
-from models.base_model import BaseModel Base
-from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
 from models.place import place_amenity
@@ -13,9 +13,4 @@ class Amenity(BaseModel, Base):
     Class amenity
     """
     __tablename__ = 'amenities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place", secondary=place_amenity)
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
